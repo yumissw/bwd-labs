cabf0c6eae20b8fed7b3f2f0627434c0ef8e3a5f
yumissw <lvlyyumiss@gmail.com>
2024-11-11 01:05:04 +0400

    feat: LAB4-3 Добавление ранне созданных страниц в проект

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 
@@ -22,32 +22,79 @@ module.exports = {
        rules: [
 
            {
-
                test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
-
                use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
-
            },
 
-       ],
 
+
+
+
+ {
+    test: /\.(png|jpg|gif|svg)$/,
+    use: [
+     {
+      loader: 'file-loader',
+ options: {
+              name: '[name].[ext]',
+              outputPath: 'smth/',
+},
+     },
+    ],
    },
 
+   
+     
+ 
+
+
+],
+},
 
    plugins: [
 
+new CopyPlugin({
+      patterns: [
+        { from: 'src/smth/ic.png', to: 'dist' }, // Скопируйте файл в папку dist
+ { from: 'src/smth/check.svg', to: 'dist' }, // Скопируйте файл в папку dist
+      ],
+    }),
+
        new HtmlWebpackPlugin({
 
-           template: './src/index.html',
+           template: './src/1.html',
 
            inject: true,
 
            chunks: ['index'],
 
-           filename: 'index.html'
+           filename: '1.html'
 
        }),
-
+new HtmlWebpackPlugin({
+          template: './src/index.html',
+          inject: true,
+          chunks: ['index'],
+          filename: 'index.html'
+        }),
+new HtmlWebpackPlugin({
+          template: './src/about.html',
+          inject: true,
+          chunks: ['index'],
+          filename: 'about.html'
+        }),
+new HtmlWebpackPlugin({
+          template: './src/main.html',
+          inject: true,
+          chunks: ['index'],
+          filename: 'main.html'
+        }),
+new HtmlWebpackPlugin({
+          template: './src/projects.html',
+          inject: true,
+          chunks: ['index'],
+          filename: 'projects.html'
+        }),
    ],
 
 
